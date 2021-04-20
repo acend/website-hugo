ed1800bc7799d132c338b39866c6131d45fd6e97
Thomas Philipona <philipona@puzzle.ch>
2021-04-20 16:50:00 +0200

    Move to plain docker setup

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,4 @@
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const path = require("path");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -15,7 +15,8 @@ module.exports = merge(common, {
   },
 
   devServer: {
-    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
+    port: process.env.PORT || 8080,
     contentBase: path.join(process.cwd(), "./dist"),
     watchContentBase: true,
     quiet: false,
