f710459dc47a26ce6c0b284d013fdfc432867aba
“tobiastroehler@gmail.com” <tobiastroehler@gmail.com>
2022-12-29 18:11:39 +0100

    UX Updates

--- src/js/home.js
+++ src/js/home.js
@@ -31,7 +31,7 @@ export default function() {
             wrapAround: true,
             fade: true,
             adaptiveHeight: false,
-            autoPlay: 5500,
+            autoPlay: 7500,
             pauseAutoPlayOnHover: false,
             prevNextButtons: false,
         });
@@ -43,7 +43,7 @@ export default function() {
             wrapAround: true,
             //fade: true,
             //adaptiveHeight: false,
-            autoPlay: 2500,
+            autoPlay: 5500,
             //pauseAutoPlayOnHover: false,
             prevNextButtons: false,
             cellAlign: 'left',
