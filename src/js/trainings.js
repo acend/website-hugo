c225ca652464582480e63cfd855f1f74a65f57f0
Fint <mail@fint.ch>
2023-01-31 15:21:15 +0100

    new filter for trainings

--- src/js/trainings.js
+++ src/js/trainings.js
@@ -3,8 +3,59 @@ export default function() {
   if($('.filterlist').length) {
 
 
-    $('.btn-filter:not(.btn-reset)').click(function() {
+      // helper function
+      function containsAll(needles, haystack) {
+        for (var i = 0, len = needles.length; i < len; i++) {
+          if ($.inArray(needles[i], haystack) === -1) {
+            return false;
+          }
+        }
+        return true;
+      }
 
+
+    const doFilter = function () {
+
+      var checked_values =  $('.btn-filter.active').map(function () {
+                          return $(this).attr('data-filter') != 'all' ? $(this).attr('data-filter') : null;
+                      }).get(); // ["cat1", "cat2", "cat3"]
+      
+            console.log(checked_values, checked_values.length)
+            
+            if (checked_values.length) {
+      
+              $('.box-training').each(function () {
+                // get all the classes from the object:
+                var catsArray = $(this).attr('class').split(' ');
+      
+                // variante 1 : alle filter müssen zutreffen:
+                if (containsAll(checked_values, catsArray)) {
+                  $(this).removeClass('d-none filtered');
+                } else {
+                  $(this).addClass('d-none filtered');
+                }
+              });
+      
+              //location.hash = checked_values.join(',');
+            }else {
+              $('.box-training').removeClass('d-none filtered');
+              //location.hash = '';
+            }
+            // triger lozad 
+            window.dispatchEvent(new Event('resize'));
+          }
+
+
+
+
+
+    $('.btn-filter:not(.btn-reset)').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active').siblings().removeClass('active');
+        doFilter();
+      });
+
+/*
       if(!$(this).hasClass('active')){
            $(this).toggleClass('active').siblings().removeClass('active');
             var selected = $('.btn-filter.active').attr('data-filter');
@@ -34,7 +85,8 @@ export default function() {
             window.history.replaceState('', document.title, noHashURL);
       }
    });
-   
+   */
+
    //activate filter on pageload with hash
    if(location.hash) {
     $('.btn-filter[data-filter="'+location.hash.split('#')[1]+'"]').trigger('click');
